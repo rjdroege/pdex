597ad73124399711bc99fc96102d132a27fff21a
rjdroege <99696219+rjdroege@users.noreply.github.com>
2022-06-04 16:04:41 -0500

    Setup

---
+++ src/app/pdex/pdex.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pdex',
+  templateUrl: './pdex.component.html',
+  styleUrls: ['./pdex.component.css']
+})
+export class PdexComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/pdex/pdex.js
@@ -0,0 +1,121 @@
+//Dom Objects
+const mainScreen = document.querySelector('.main-screen');
+const pokeName = document.querySelector('.poke-name');
+const pokeId = document.querySelector('.poke-id');
+const pokeFrontImage = document.querySelector('.poke-front-image');
+const pokeBackImage = document.querySelector('.poke-back-image');
+const pokeTypeOne = document.querySelector('.poke-type-one');
+const pokeTypeTwo = document.querySelector('.poke-type-two');
+const pokeWeight = document.querySelector('.poke-weight');
+const pokeHeight = document.querySelector('.poke-height');
+const pokeListItems = document.querySelectorAll ('.list-item');
+const leftButton = document.querySelector('.left-button');
+const rightButton = document.querySelector('.right-button');
+
+//constants & variables
+const TYPES = [
+  'normal', 'fighting', 'flying', 'poison', 'ground', 'rock',
+  'bug', 'ghost', 'steel', 'fire', 'water', 'grass', 'electric',
+  'psychic', 'ice', 'dragon', 'dark', 'fairy'
+];
+let preUrl = 'null';
+let nextUrl = 'null';
+
+//Functions
+const capitalize = (str) => str[0].toUpperCase() + str.substr(1);
+
+const resetScreen = () => {
+  mainScreen.classList.remove('hide');
+  for (const type of TYPES) {
+    mainScreen.classList.remove(type);
+  }
+};
+
+
+const fetchPokeList = url => {
+  fetch(url)
+.then(res => res.json())
+.then(data => {
+  const {results, previous, next } = data;
+  preUrl = previous;
+  nextUrl = next;
+
+  for (let i = 0; i < pokeListItems.length ; i++) {
+    const pokeListItem = pokeListItems [i];
+    const resultData = results[i];
+
+    if (resultData) {
+      const { name, url } = resultData;
+      const urlArray = url.split('/');
+      const id = urlArray[urlArray.length - 2];
+      pokeListItem.textContent = id + '.' + capitalize(name);
+    } else{
+      pokeListItem.textContent = '';
+
+    }
+  }
+
+});
+};
+
+const fetchPokeData = id => {
+  fetch('https://pokeapi.co/api/v2/pokemon/${id}')
+  .then(res => res.json())
+  .then(data => {
+  resetScreen();
+
+    const dataTypes = data['types'];
+    const dataFirstType = dataTypes = dataTypes[0];
+    const dataSecondType = dataTypes = dataTypes[1];
+    pokeTypeOne.textContent = capitalize (dataFirstType[0]['type']['name']);
+    if (dataSecondType) {
+      pokeTypeTwo.classList.remove('hide');
+      pokeTypeTwo.textContent = capitalize (dataSecondType['type']['name']);
+    } else {
+      pokeTypeTwo.classList.add('hide');
+      pokeTypeTwo.textContent = '';
+    }
+    mainScreen.classList.add(dataFirstType['type']['name']);
+
+    pokeName.textContent = capitalize (data['name']);
+    pokeId.textContent = '#' + data['id'].toString().padStart(3, '0');
+    pokeWeight.textContent = data['weight'];
+    pokeHeight.textContent = data['height'];
+    pokeFrontImage.src = data['sprites']['front_default'];
+    pokeBackImage.src = data['sprites']['back_default'];
+  });
+};
+
+const handleLeftButtonClick = () => {
+  if (preUrl) {
+    fetchPokeList(preUrl);
+  }
+};
+
+const handleRightButtonClick = () => {
+  if (nextUrl) {
+    fetchPokeList(nextUrl);
+  }
+
+};
+
+const handleListItemClick = (e) => {
+  if (!e.target) return;
+
+  const ListItem = e.target;
+  if(!ListItem.textContent) return;
+
+  const textArray = ListItem.textContent.split('.')[0];
+  fetchPokeData(id);
+};
+
+
+//Adding event listeners
+leftButton.addEventListener('click', handleLeftButtonClick);
+rightButton.addEventListener('click', handleRightButtonClick);
+for (const pokeListItem of pokeListItems) {
+  pokeListItem.addEventListener('click', handleListItemClick);
+};
+
+//Initalize App
+fetchPokeList('https://pokeapi.co/api/v2/pokemon?offset=0&limit=20');
