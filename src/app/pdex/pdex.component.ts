a6eebfad970a67b32d3f9a8cf7836f393327a8ca
rjdroege <99696219+rjdroege@users.noreply.github.com>
2022-06-05 12:09:08 -0500

    starting API script

--- src/app/pdex/pdex.component.ts
+++ src/app/pdex/pdex.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-pdex',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pdex.component.css']
 })
 export class PdexComponent implements OnInit {
+finalArray= [];
 
-  constructor() { }
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.getPokemon();
+  }
+
+  getPokemon() {
+    this.http.get<{}>('https://pokeapi.co/api/v2/pokemon/').subscribe((res) => {
+      console.log(res);
+    });
   }
 
 }
