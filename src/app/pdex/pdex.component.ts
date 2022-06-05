70d325670e870144992cd46d3dfc8b7507f901d2
rjdroege <99696219+rjdroege@users.noreply.github.com>
2022-06-05 12:15:45 -0500

    updating API Array

--- src/app/pdex/pdex.component.ts
+++ src/app/pdex/pdex.component.ts
@@ -17,8 +17,12 @@ finalArray= [];
   }
 
   getPokemon() {
-    this.http.get<{}>('https://pokeapi.co/api/v2/pokemon/').subscribe((res) => {
-      console.log(res);
+    this.http.get<{}>('https://pokeapi.co/api/v2/pokemon/').subscribe((pokes) => {
+      const allPokes = Object.assign(pokes);
+      const pokesArray = Object.entries(allPokes);
+      const apiPokes: any = pokesArray[3];
+      this.finalArray = apiPokes[1];
+      console.log(this.finalArray);
     });
   }
 
