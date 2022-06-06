f9ec1fece49352f1e4df07a483899d0f979ec88d
Michael030906 <99698526+Michael030906@users.noreply.github.com>
2022-06-06 18:02:22 -0500

    Add files via upload

---
+++ pdex.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Pokemon } from './pokemon.model';
+
+@Component({
+  selector: 'app-pdex',
+  templateUrl: './pdex.component.html',
+  styleUrls: ['./pdex.component.css']
+})
+export class PdexComponent implements OnInit {
+finalArray= [];
+
+test = '';
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getPokemon();
+  }
+
+  getPokemon() {
+    this.http.get<{}>('https://pokeapi.co/api/v2/pokemon/').subscribe((pokes) => {
+      const allPokes = Object.assign(pokes);
+      const apiPokesArray = Object.entries(allPokes);
+      const pokesArray: any = apiPokesArray[3][1];
+      console.log(pokesArray);
+
+
+    });
+  }
+
+}
