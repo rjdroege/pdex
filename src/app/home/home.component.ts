4db1bf1bbe7fd18ceda0d3ad5733669088fdfe8a
rjdroege <99696219+rjdroege@users.noreply.github.com>
2022-06-09 18:11:25 -0500

    API tests

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+  }
+
+  callPokes() {
+    this.http.get('https://pokeapi.co/api/v2/pokemon/bulbasaur/').subscribe((res) => {
+      console.log(res);
+    })
   }
 
 }
