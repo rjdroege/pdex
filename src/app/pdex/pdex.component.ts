3e4ce01612824ad9ade1ee5a19fb6936ca359901
Michael030906 <mdscool03@gmail.com>
2022-06-11 16:10:46 -0500

    Update pdex.component.ts

--- src/app/pdex/pdex.component.ts
+++ src/app/pdex/pdex.component.ts
@@ -9,6 +9,13 @@ import { Pokemon } from './pokemon.model';
 })
 export class PdexComponent implements OnInit {
 finalArray= [];
+<<<<<<< Updated upstream
+=======
+savedArray= [];
+//rough description for pokemon not sorted yet
+descObj = [];
+
+>>>>>>> Stashed changes
 
 
   constructor(private http: HttpClient) { }
@@ -25,11 +32,30 @@ finalArray= [];
       console.log(pokesArray);
     });
   }
+<<<<<<< Updated upstream
 
   pokeAbility() {
     this.http.get('https://pokeapi.co/api/v2/ability/1/').subscribe((res) => {
 
     })
   }
+=======
+  //finds the url for click
+   pokeClickId(idx: number) {
+      const thisPoke: string = this.finalArray[idx].name;
+        this.http.get<{}>(`https://pokeapi.co/api/v2/pokemon/${thisPoke}`).subscribe((res) => {
+          const allPokes = Object.assign(res);
+          const apiPokesArray = Object.entries(allPokes);
+            this.descObj = apiPokesArray
+      })
+      this.pokeDescSort()
+    }
+//sort through api data and returns it
+    pokeDescSort(){
+     let help = Object.values(this.descObj[0][1][0])
+     let help2 = Object.values(help[0])
+     console.log(help2[0])
+    }
+>>>>>>> Stashed changes
 
 }
