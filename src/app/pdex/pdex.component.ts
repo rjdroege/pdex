ef88ff04f6ae9421bc941b762f7be8c0eda75aca
Michael030906 <mdscool03@gmail.com>
2022-06-11 16:57:16 -0500

    WIP on description

--- src/app/pdex/pdex.component.ts
+++ src/app/pdex/pdex.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Pokemon } from './pokemon.model';
 
 @Component({
   selector: 'app-pdex',
@@ -9,53 +8,51 @@ import { Pokemon } from './pokemon.model';
 })
 export class PdexComponent implements OnInit {
 finalArray= [];
-<<<<<<< Updated upstream
-=======
 savedArray= [];
-//rough description for pokemon not sorted yet
-descObj = [];
+userArray= [];
+descObj = []
 
->>>>>>> Stashed changes
-
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient){ }
 
   ngOnInit(): void {
     this.getPokemon();
   }
 
   getPokemon() {
-    this.http.get<{}>('https://pokeapi.co/api/v2/pokemon').subscribe((pokes) => {
+    this.http.get<{}>('https://pokeapi.co/api/v2/pokemon/').subscribe((pokes) => {
       const allPokes = Object.assign(pokes);
       const apiPokesArray = Object.entries(allPokes);
       const pokesArray: any = apiPokesArray[3][1];
-      console.log(pokesArray);
+      this.finalArray = pokesArray
+      console.log(this.finalArray)
+
     });
   }
-<<<<<<< Updated upstream
-
-  pokeAbility() {
-    this.http.get('https://pokeapi.co/api/v2/ability/1/').subscribe((res) => {
-
-    })
-  }
-=======
   //finds the url for click
-   pokeClickId(idx: number) {
+    pokeClickId(idx: number) {
       const thisPoke: string = this.finalArray[idx].name;
-        this.http.get<{}>(`https://pokeapi.co/api/v2/pokemon/${thisPoke}`).subscribe((res) => {
-          const allPokes = Object.assign(res);
-          const apiPokesArray = Object.entries(allPokes);
-            this.descObj = apiPokesArray
+      this.http.get<{}>(`https://pokeapi.co/api/v2/pokemon/${thisPoke}`).subscribe((res) => {
+        const allPokes = Object.assign(res);
+        const apiPokesArray = Object.entries(allPokes);
+         this.descObj = apiPokesArray
       })
+      console.log(this.descObj)
       this.pokeDescSort()
     }
-//sort through api data and returns it
-    pokeDescSort(){
-     let help = Object.values(this.descObj[0][1][0])
-     let help2 = Object.values(help[0])
-     console.log(help2[0])
+
+    savePoke(idx: number) {
+      this.userArray = this.savedArray[idx];
     }
->>>>>>> Stashed changes
+
+    deletePoke(idx: number) {
+      this.userArray = [];
+    }
+
+   //sort through api data and returns it
+   pokeDescSort(){
+    let help = Object.values(this.descObj[0][1][0])
+    let help2 = Object.values(help[0])
+    console.log(help2[0])
+   }
 
 }
