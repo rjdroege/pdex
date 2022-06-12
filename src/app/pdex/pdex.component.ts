29127211f02c11d5d7bc055f444bb5ce983686a0
Michael030906 <mdscool03@gmail.com>
2022-06-12 03:49:49 -0500

    Big Update

--- src/app/pdex/pdex.component.ts
+++ src/app/pdex/pdex.component.ts
@@ -21,33 +21,60 @@ descData = {
 };
 descArry = [];
 
+url = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20'
 
+nextUrl:any = '';
+
+prevUrl:any = '';
 
   constructor(private http: HttpClient){ }
 
   ngOnInit(): void {
     this.getPokemon();
+    // this.prevUrl = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20'
   }
 
+  // getPokemon() {
+  //   this.http.get<{}>('https://pokeapi.co/api/v2/pokemon/heracross-mega').subscribe((pokes) => {
+  //     const allPokes = Object.assign(pokes);
+  //     const apiPokesArray = Object.entries(allPokes);
+  //     const pokesArray: any = apiPokesArray[3][1];
+  //     console.log
+  //   });
+  // }
+
   getPokemon() {
-    this.http.get<{}>('https://pokeapi.co/api/v2/pokemon/').subscribe((pokes) => {
+    this.http.get<{}>(this.url).subscribe((pokes) => {
       const allPokes = Object.assign(pokes);
       const apiPokesArray = Object.entries(allPokes);
       const pokesArray: any = apiPokesArray[3][1];
       this.finalArray = pokesArray
-      console.log(this.finalArray)
+      this.nextUrl = apiPokesArray[1][1]
+      this.prevUrl = apiPokesArray[2][1]
+      console.log(apiPokesArray[2])
     });
   }
+
+  nextBtn(){
+    this.url = this.nextUrl
+    this.getPokemon()
+  }
+
+  prevBtn(){
+    this.url = this.prevUrl
+    this.getPokemon()
+  }
+
   //finds the url for click
-    pokeClickId(idx: number) {
+   pokeClickId(idx: number) {
       const thisPoke: string = this.finalArray[idx].name;
-      this.http.get<{}>(`https://pokeapi.co/api/v2/pokemon/${thisPoke}`).subscribe((res) => {
+       this.http.get<{}>(`https://pokeapi.co/api/v2/pokemon/${thisPoke}`).subscribe((res) => {
         const allPokes = Object.assign(res);
         const apiPokesArray = Object.entries(allPokes);
          this.descObj = apiPokesArray
+         this.mainDescCall()
+         console.log(this.descData)
       })
-      console.log(this.descObj)
-      this.mainDescCall()
     }
 
     savePoke(idx: number) {
@@ -59,13 +86,11 @@ descArry = [];
     }
 //general call to all description functions for click
     mainDescCall(){
-      //this.pokeDescSortAbilities()
-      //this.pokeDescSortHeight()
-      //this.pokeDescSortName()
+      this.pokeDescSortAbilities()
       this.pokeDescSortTypes()
-      //this.pokeDescSortWeight()
-      //this.descArry = Object.entries(this.descData)
-      //console.log(this.descArry)
+      this.pokeDescSortWNH(4,"height")
+      this.pokeDescSortWNH(10, "name")
+      this.pokeDescSortWNH(17, "weight")
     }
 
    //sort through api data and returns it for abilities
@@ -73,7 +98,8 @@ descArry = [];
     //array we push to descData
     let abilArry = []
     //if statement to catch the number of abilites might have to change soon
-    if(this.descObj[0][1].length = 2){
+    console.log(this.descObj[0])
+    if(this.descObj[0].length === 2){
       //first ability to be pushed
     let abil1 = Object.entries(this.descObj[0][1][0])
     let abil1NameObj = Object.entries(abil1)
@@ -109,67 +135,49 @@ descArry = [];
     //array we push to descData
     let typeArry = []
     //if statement to catch the number of abilites might have to change soon
-    if(this.descObj[16][1].length = 2){
+    if(this.descObj[16][1].length === 2){
       //first ability to be pushed
-    let abil1 = Object.entries(this.descObj[16][1][0]);
-    console.log(abil1)
-    let abil1NameObj = Object.entries(abil1[1])
-    console.log(abil1NameObj)
-    let abil1Name = Object.entries(abil1NameObj[1][1])
-    console.log(abil1Name)
+    let type1 = Object.entries(this.descObj[16][1][0]);
+
+    let type1NameObj = Object.entries(type1[1])
+
+    let type1Name = Object.entries(type1NameObj[1][1])
       //pushing first ability name
-    typeArry.push(abil1Name[0][1])
+    typeArry.push(type1Name[0][1])
 
       //second ability to be pushed
-    let abil2 = Object.entries(this.descObj[16][1][1])
-    let abil2NameObj = Object.entries(abil2[1])
-    console.log(abil2NameObj)
-    let abil2Name = Object.values(abil2NameObj[1][1])
+    let type2 = Object.entries(this.descObj[16][1][1])
+    let type2NameObj = Object.entries(type2[1])
+
+    let type2Name = Object.values(type2NameObj[1][1])
       //pushing first ability name
 
-    typeArry.push(abil2Name[0])
+    typeArry.push(type2Name[0])
       //setting array with values in it into descData
     this.descData.types = typeArry
     }
     else{
-        //first ability to be pushed
-      let abil1 = Object.entries(this.descObj[16][1][0])
-      let abil1NameObj = Object.entries(abil1)
-      let abil1Name = Object.entries(abil1NameObj[0][1][1])
-        //pushing first ability name
-      typeArry.push(abil1Name[0][1])
-        //setting array with values in it into descData
-      // this.descData.types = typeArry
-      console.log(typeArry)
+       //first ability to be pushed
+       console.log(this.descObj[16][1][0])
+       let type1 = Object.entries(this.descObj[16][1][0]);
+
+       let type1NameObj = Object.entries(type1[1])
+
+       let type1Name = Object.entries(type1NameObj[1][1])
+         //pushing first ability name
+       typeArry.push(type1Name[0][1])
+
+      this.descData.types = typeArry
     }
     }
-//can combine height and name by using parameters
 
-   //finds height from api call and puts it into descData
-   pokeDescSortHeight(){
-    //finds height from array and makes some objects arrays
-    let heightObj = Object.entries(this.descObj[4])
-    let height = Object.values(heightObj[1])
-    //pushes height to descData height
-    this.descData.height.push(height[1])
-   }
-//want to combine height and name code since so simmilar
-   pokeDescSortName(){
-    let nameObj = Object.entries(this.descObj[10])
 
-    let name = Object.values(nameObj[1])
+   //Sorts weight,name, and height
+   pokeDescSortWNH(num: number, properties:string){
+    let Obj = Object.entries(this.descObj[num])
 
-    this.descData.name.push(name[1])
-   }
+    let value = Object.values(Obj[1])
 
-   //want to combine height and name code since so simmilar
-   pokeDescSortWeight(){
-    let weightObj = Object.entries(this.descObj[17])
-
-    let weight = Object.values(weightObj[1])
-
-    this.descData.weight.push(weight[1])
-
-    console.log(this.descData)
+    this.descData[properties] = value[1]
    }
 }
