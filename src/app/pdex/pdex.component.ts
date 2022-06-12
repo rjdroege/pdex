4e814f382bae9a294fea162b6ad9009b601326f1
Michael030906 <mdscool03@gmail.com>
2022-06-12 00:01:00 -0500

    Big change

--- src/app/pdex/pdex.component.ts
+++ src/app/pdex/pdex.component.ts
@@ -7,10 +7,21 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./pdex.component.css']
 })
 export class PdexComponent implements OnInit {
+
 finalArray= [];
 savedArray= [];
 userArray= [];
-descObj = []
+descObj = [];
+descData = {
+  "abilities":[],
+  "height":[],
+  "name":[],
+  "types":[],
+  "weight":[]
+};
+descArry = [];
+
+
 
   constructor(private http: HttpClient){ }
 
@@ -36,7 +47,7 @@ descObj = []
          this.descObj = apiPokesArray
       })
       console.log(this.descObj)
-      this.pokeDescSort()
+      this.mainDescCall()
     }
 
     savePoke(idx: number) {
@@ -46,12 +57,119 @@ descObj = []
     deletePoke(idx: number) {
       this.userArray = [];
     }
+//general call to all description functions for click
+    mainDescCall(){
+      //this.pokeDescSortAbilities()
+      //this.pokeDescSortHeight()
+      //this.pokeDescSortName()
+      this.pokeDescSortTypes()
+      //this.pokeDescSortWeight()
+      //this.descArry = Object.entries(this.descData)
+      //console.log(this.descArry)
+    }
 
-   //sort through api data and returns it
-   pokeDescSort(){
-    let help = Object.values(this.descObj[0][1][0])
-    let help2 = Object.values(help[0])
-    console.log(help2[0])
+   //sort through api data and returns it for abilities
+   pokeDescSortAbilities(){
+    //array we push to descData
+    let abilArry = []
+    //if statement to catch the number of abilites might have to change soon
+    if(this.descObj[0][1].length = 2){
+      //first ability to be pushed
+    let abil1 = Object.entries(this.descObj[0][1][0])
+    let abil1NameObj = Object.entries(abil1)
+    let abil1Name = Object.entries(abil1NameObj[0][1][1])
+      //pushing first ability name
+    abilArry.push(abil1Name[0][1])
+
+      //first ability to be pushed
+    let abil2 = Object.entries(this.descObj[0][1][1])
+    let abil2NameObj = Object.entries(abil2)
+    let abil2Name = Object.values(abil2NameObj[0][1][1])
+      //pushing first ability name
+    abilArry.push(abil2Name[0])
+      //setting array with values in it into descData
+    this.descData.abilities = abilArry
+    }
+    else{
+        //first ability to be pushed
+      let abil1 = Object.entries(this.descObj[0][1][0])
+      let abil1NameObj = Object.entries(abil1)
+      let abil1Name = Object.entries(abil1NameObj[0][1][1])
+        //pushing first ability name
+      abilArry.push(abil1Name[0][1])
+        //setting array with values in it into descData
+      this.descData.abilities = abilArry
+    }
    }
 
+
+
+  //sort through api data and returns it for abilities
+  pokeDescSortTypes(){
+    //array we push to descData
+    let typeArry = []
+    //if statement to catch the number of abilites might have to change soon
+    if(this.descObj[16][1].length = 2){
+      //first ability to be pushed
+    let abil1 = Object.entries(this.descObj[16][1][0]);
+    console.log(abil1)
+    let abil1NameObj = Object.entries(abil1[1])
+    console.log(abil1NameObj)
+    let abil1Name = Object.entries(abil1NameObj[1][1])
+    console.log(abil1Name)
+      //pushing first ability name
+    typeArry.push(abil1Name[0][1])
+
+      //second ability to be pushed
+    let abil2 = Object.entries(this.descObj[16][1][1])
+    let abil2NameObj = Object.entries(abil2[1])
+    console.log(abil2NameObj)
+    let abil2Name = Object.values(abil2NameObj[1][1])
+      //pushing first ability name
+
+    typeArry.push(abil2Name[0])
+      //setting array with values in it into descData
+    this.descData.types = typeArry
+    }
+    else{
+        //first ability to be pushed
+      let abil1 = Object.entries(this.descObj[16][1][0])
+      let abil1NameObj = Object.entries(abil1)
+      let abil1Name = Object.entries(abil1NameObj[0][1][1])
+        //pushing first ability name
+      typeArry.push(abil1Name[0][1])
+        //setting array with values in it into descData
+      // this.descData.types = typeArry
+      console.log(typeArry)
+    }
+    }
+//can combine height and name by using parameters
+
+   //finds height from api call and puts it into descData
+   pokeDescSortHeight(){
+    //finds height from array and makes some objects arrays
+    let heightObj = Object.entries(this.descObj[4])
+    let height = Object.values(heightObj[1])
+    //pushes height to descData height
+    this.descData.height.push(height[1])
+   }
+//want to combine height and name code since so simmilar
+   pokeDescSortName(){
+    let nameObj = Object.entries(this.descObj[10])
+
+    let name = Object.values(nameObj[1])
+
+    this.descData.name.push(name[1])
+   }
+
+   //want to combine height and name code since so simmilar
+   pokeDescSortWeight(){
+    let weightObj = Object.entries(this.descObj[17])
+
+    let weight = Object.values(weightObj[1])
+
+    this.descData.weight.push(weight[1])
+
+    console.log(this.descData)
+   }
 }
