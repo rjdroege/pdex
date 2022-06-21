ba95222a169db1e694b05ef646dc799ce982de97
Michael030906 <mdscool03@gmail.com>
2022-06-21 10:58:02 -0500

    Added delete feature to favorites

--- src/app/shared/fav-data.service.ts
+++ src/app/shared/fav-data.service.ts
@@ -15,19 +15,15 @@ export class FavDataService {
   storedArryGet = JSON.parse(localStorage.getItem("mainArray"))
 
 
-  // callStorage(){
-  //   this.mainArry = this.storedArryGet
-  // }
+  callStorage(){
+    this.mainArry = this.storedArryGet
+  }
+
   saveInput(idx) {
-    if(idx === null){
-      this.mainArry = this.storedArryGet
-      console.log("works")
-      return
-    }
-    else if(this.mainArry.length === 0) {
-      this.mainArry = this.storedArryGet
-      this.mainArry.push(idx);
+    if(this.mainArry.length === 0) {
       localStorage.setItem("mainArray",JSON.stringify(this.mainArry))
+      this.mainArry.push(idx);
+      console.log(this.mainArry)
       return
     }else{
     let clear = true;
@@ -38,22 +34,20 @@ export class FavDataService {
     });
     if (clear) {
       this.mainArry.push(idx);
-      localStorage.setItem("mainArray",JSON.stringify(this.mainArry))
       return
       } else {
        console.log('You already liked that Joke!')
-       return
-      }
-  }}
+
+      }}
+    localStorage.setItem("mainArray",JSON.stringify(this.mainArry))
+    return
+  }
 
  deleteFavPoke(idx){
     let string  = this.mainArry.toString()
     let array = string.split(",")
-    let array2 = array.filter(data =>
-       data = idx);
-       console.log(array2)
+    let array2 = array.filter(function(data){return data != idx})
     this.mainArry = array2
-    console.log(this.mainArry)
     localStorage.setItem("mainArray",JSON.stringify(this.mainArry))
     return
  }
